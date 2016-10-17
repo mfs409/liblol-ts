d88b7a165b483cb1521fd1ec6d73cbeb54f37efb
Michael Spear <spear@cse.lehigh.edu>
2016-10-16 23:24:46 -0400

    removed parts of Media that don't apply anymore

--- src/liblol-ts/Media.ts
+++ src/liblol-ts/Media.ts
@@ -1,3 +1,5 @@
+// TODO: conversion of this file is not yet complete
+
 // /**
 //  * This is free and unencumbered software released into the public domain.
 //  *
@@ -162,89 +164,4 @@
 //         }
 //     }
 
-//     /*
-//      * PUBLIC INTERFACE
-//      */
-
-//     /**
-//      * Register an image file, so that it can be used later. Images should be
-//      * .png files. Note that images with internal animations (i.e., gifs) do not
-//      * work correctly. You should use cell-based animation instead.
-//      *
-//      * @param imgName the name of the image file (assumed to be in the "assets"
-//      *                folder). This should be of the form "image.png", and should be
-//      *                of type "png". "jpeg" images work too, but usually look bad in
-//      *                games
-//      */
-//     static public void registerImage(String imgName) {
-//         // Create an array with one entry
-//         TextureRegion[] tr = new TextureRegion[1];
-//         tr[0] = new TextureRegion(new Texture(Gdx.files.internal(imgName)));
-//         Lol.sGame.mMedia.mImages.put(imgName, tr);
-//     }
-
-//     /**
-//      * Register an animatable image file, so that it can be used later. The
-//      * difference between regular images and animatable images is that
-//      * animatable images should be thought of as having multiple columns and
-//      * rows, which allow cell-based animation. Images should be .png files. Note
-//      * that images with internal animations (i.e., gifs) do not work correctly.
-//      * You should use cell-based animation instead.
-//      *
-//      * @param imgName the name of the image file (assumed to be in the "assets"
-//      *                folder). This should be of the form "image.png", and should be
-//      *                of type "png"
-//      * @param columns The number of columns that comprise this image file
-//      * @param rows    The number of rows that comprise this image file
-//      */
-//     static public void registerAnimatableImage(String imgName, int columns, int rows) {
-//         // Load the file as a texture
-//         Texture t = new Texture(Gdx.files.internal(imgName));
-//         // carve the image into cells of equal width and height
-//         int width = t.getWidth() / columns;
-//         int height = t.getHeight() / rows;
-//         TextureRegion[][] trgrid = TextureRegion.split(t, width, height);
-//         // put all entries into a 1-D array
-//         TextureRegion[] trs = new TextureRegion[columns * rows];
-//         int index = 0;
-//         for (int i = 0; i < rows; ++i) {
-//             for (int j = 0; j < columns; ++j) {
-//                 trs[index] = trgrid[i][j];
-//                 index++;
-//             }
-//         }
-//         Lol.sGame.mMedia.mImages.put(imgName, trs);
-//     }
-
-//     /**
-//      * Register a music file, so that it can be used later. Music should be in
-//      * .ogg format. You can use Audacity to convert music as needed. mp3 files
-//      * should work too, but may have licensing restrictions in some countries.
-//      *
-//      * @param musicName the name of the music file (assumed to be in the "assets"
-//      *                  folder). This should be of the form "song.ogg", and should be
-//      *                  of type "ogg".
-//      * @param loop      either true or false, to indicate whether the song should
-//      *                  repeat when it reaches the end
-//      */
-//     static public void registerMusic(String musicName, boolean loop) {
-//         Music m = Gdx.audio.newMusic(Gdx.files.internal(musicName));
-//         m.setLooping(loop);
-//         m.setVolume(Facts.getGameFact("volume", 1));
-//         Lol.sGame.mMedia.mTunes.put(musicName, m);
-//     }
-
-//     /**
-//      * Register a sound file, so that it can be used later. Sounds should be
-//      * .ogg files. You can use Audacity to convert sounds as needed. mp3 files
-//      * should work too
-//      *
-//      * @param soundName the name of the sound file (assumed to be in the "assets"
-//      *                  folder). This should be of the form "sound.ogg", and should be
-//      *                  of type "ogg".
-//      */
-//     static public void registerSound(String soundName) {
-//         Sound s = Gdx.audio.newSound(Gdx.files.internal(soundName));
-//         Lol.sGame.mMedia.mSounds.put(soundName, s);
-//     }
 // }
